a7ec25d427216492d4aef35fbb42d994065da431
BuildTools <unconfigured@null.spigotmc.org>
2020-11-02 14:32:13 -0300

    Test commit

--- assets/js/clases.js
+++ assets/js/clases.js
@@ -28,7 +28,7 @@ class Carrito{
     agregar_compra(producto, cantidad){
         if (producto.renovar_cantidad(cantidad)){
             this.compras.push(producto);
-            return true;
+            return 1;
         } else {
             return false;
         }
